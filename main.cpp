1dbae9e8371c2e2dba3fda71e28c7f3e91f9e273
Michal <michgluacad7@gmail.com>
2025-03-18 12:29:32 +0100

    Added simple Peterson Algorithm for synchronization

--- main.cpp
+++ main.cpp
@@ -1,55 +1,60 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <atomic>
+#include <chrono>
 
-class Philosopher {
-public:
-    Philosopher(int id) : id(id) {}
-    void dine();
+using namespace std;
 
-private:
-    int id;
-    void think();
-    void eat();
-};
+const int MAX_PHILOSOPHERS = 10;
+atomic<bool> flag[MAX_PHILOSOPHERS];
+atomic<int> turn;
+int num_philosophers;
 
-void Philosopher::dine() {
-    while (true) {
-        think();
-        eat();
+void philosopher(int id) {
+    for (int i = 0; i < 5; ++i) { // Each philosopher is trying to each 5 times
+        // Entrance to critical section
+        flag[id] = true;
+        turn = id;
+        for (int j = 0; j < num_philosophers; ++j) {
+            if (j != id) {
+                while (flag[j] && turn == id);
+            }
+        }
+
+        // Critical section - eating
+        cout << "Philosopher " << id << " is eating.\n";
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        // Exit from critical section
+        flag[id] = false;
+        cout << "Philosopher " << id << " finished eating and is thinking.\n";
     }
-}
-
-void Philosopher::think() {
-    std::cout << "Philosopher " << id << " is thinking.\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-}
-
-void Philosopher::eat() {
-    std::cout << "Philosopher " << id << " is eating.\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <number_of_philosophers>\n";
+        cerr << "Usage: " << argv[0] << " <number_of_philosophers>\n";
         return 1;
     }
 
-    int numPhilosophers = std::stoi(argv[1]);
-    std::vector<std::thread> threads;
-    std::vector<Philosopher> philosophers;
-
-    for (int i = 0; i < numPhilosophers; ++i) {
-        philosophers.emplace_back(i);
+    num_philosophers = stoi(argv[1]);
+    if (num_philosophers > MAX_PHILOSOPHERS || num_philosophers < 2) {
+        cerr << "Number of philosophers must be between 2 and " << MAX_PHILOSOPHERS << "\n";
+        return 1;
     }
 
-    for (int i = 0; i < numPhilosophers; ++i) {
-        threads.emplace_back(&Philosopher::dine, &philosophers[i]);
+    vector<thread> philosophers;
+    for (int i = 0; i < num_philosophers; ++i) {
+        flag[i] = false;
     }
 
-    for (auto& t : threads) {
-        t.join();
+    for (int i = 0; i < num_philosophers; ++i) {
+        philosophers.emplace_back(philosopher, i);
+    }
+
+    for (auto& phil : philosophers) {
+        phil.join();
     }
 
     return 0;
